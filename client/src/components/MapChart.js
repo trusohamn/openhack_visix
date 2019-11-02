0be2ca3ba3c3fc2025050ebcc0ee87291044f6d2
Mart Trusohamn <marta.trusohamn@gmail.com>
2019-11-02 08:44:08 +0100

    zoomable

--- client/src/components/MapChart.js
+++ client/src/components/MapChart.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { csv } from "d3-fetch";
-import { scaleLinear } from "d3-scale";
+// import { scaleLinear } from "d3-scale";
 import {
+  ZoomableGroup,
   ComposableMap,
   Geographies,
   Geography,
@@ -9,6 +10,16 @@ import {
   Graticule
 } from "react-simple-maps";
 import rgbHex from "rgb-hex";
+
+const rounded = num => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + "Bn";
+  } else if (num > 1000000) {
+    return Math.round(num / 100000) / 10 + "M";
+  } else {
+    return Math.round(num / 100) / 10 + "K";
+  }
+};
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
@@ -39,31 +50,33 @@ const MapChart = () => {
         scale: 147
       }}
     >
-      <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      {data.length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => {
-              const d = data.find(s => s.ISO3 === geo.properties.ISO_A3);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={
-                    d
-                      ? colorScale2D(d["corruption"], d["investment"])
-                      : "#F5F4F6"
-                  }
-                  onClick={() => {
-                    console.log(geo.properties.ABBREV);
-                  }}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
+      <ZoomableGroup>
+        <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+        {data.length > 0 && (
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map(geo => {
+                const d = data.find(s => s.ISO3 === geo.properties.ISO_A3);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={
+                      d
+                        ? colorScale2D(d["corruption"], d["investment"])
+                        : "#F5F4F6"
+                    }
+                    onClick={() => {
+                      console.log(geo.properties.ABBREV);
+                    }}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        )}
+      </ZoomableGroup>
     </ComposableMap>
   );
 };
