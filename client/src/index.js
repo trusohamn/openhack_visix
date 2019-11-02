a224744355e91ab359878250289a24abbdc740f6
Mart Trusohamn <marta.trusohamn@gmail.com>
2019-11-02 09:58:23 +0100

    tabs

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import ReactTooltip from "react-tooltip";
-
+import { Tabs, Tab } from "react-bootstrap";
 import "./styles.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import MapChart from "./components/MapChart";
 
 function App() {
   const [content, setContent] = useState("");
   return (
     <div>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tab eventKey="public" title="Public"></Tab>
+        <Tab eventKey="education" title="Education"></Tab>
+        <Tab eventKey="health" title="Health"></Tab>
+      </Tabs>
       <MapChart setTooltipContent={setContent} />
       <ReactTooltip>{content}</ReactTooltip>
     </div>
