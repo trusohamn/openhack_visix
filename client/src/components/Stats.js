727b2a986385aad54af365168d53a17a68cb1e7d
Mart Trusohamn <marta.trusohamn@gmail.com>
2019-11-02 18:02:13 +0100

    education works

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,26 +1,38 @@
 import React, { useEffect } from "react";
 
 export default ({ data }) => {
-  const corruption = 'Corruption risks'
-  const name = 'Country Name'
+  const corruption = "Corruption risks";
+  const name = "Country Name";
 
-  const mostCorrupted = data.sort((c1, c2) => c1[corruption] <= c2[corruption] ? 1 : -1).slice(0,5)
-  const mostInvested = data.sort((c1, c2) => c1.investment <= c2.investment ? 1 : -1).slice(0,5)
-  const lessInvested = data.sort((c1, c2) => c1.investment >= c2.investment ? 1 : -1).slice(0,5)
-
+  const mostCorrupted = data
+    .sort((c1, c2) => (c1[corruption] <= c2[corruption] ? 1 : -1))
+    .slice(0, 5);
+  const mostInvested = data
+    .sort((c1, c2) => (c1.investment <= c2.investment ? 1 : -1))
+    .slice(0, 5);
+  const lessInvested = data
+    .sort((c1, c2) => (c1.investment >= c2.investment ? 1 : -1))
+    .slice(0, 5);
+  console.log("***********", mostCorrupted);
   return (
     <div className="content Stats">
       most corrupted
       <ul>
-      {mostCorrupted.map(country => <li>{country[name]}</li>)}
+        {mostCorrupted.map(country => (
+          <li>{country[name]}</li>
+        ))}
       </ul>
       most invested
       <ul>
-      {mostInvested.map(country => <li>{country[name]}</li>)}
+        {mostInvested.map(country => (
+          <li>{country[name]}</li>
+        ))}
       </ul>
       least invested
       <ul>
-      {lessInvested.map(country => <li>{country[name]}</li>)}
+        {lessInvested.map(country => (
+          <li>{country[name]}</li>
+        ))}
       </ul>
     </div>
   );
